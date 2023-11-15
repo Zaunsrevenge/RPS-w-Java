4fdc00a09d3b9b3a93ef6f6322dd80d2dfc0aff8
Zaunsrevenge <123345261+Zaunsrevenge@users.noreply.github.com>
2023-11-15 04:13:08 +0000

    rules added game completed

--- script.js
+++ script.js
@@ -22,4 +22,38 @@ function selectChoice() {
     //random for opponent       
     opponent = choices[Math.floor(Math.random() * 3)]; //0- .999999 * 3 = 0-2.99999
     document.getElementById("opponent-choice").src = opponent + ".png";
+
+    //check for winner
+    if (you == opponent) {
+        yourScore += 1;
+        opponentScore += 1;
+    }
+    else {
+        if (you == "rock") {
+            if (opponent == "scissors") {
+                yourScore += 1;
+            }
+            else if (opponent == "paper") {
+                opponentScore += 1;
+            }
+        }
+        else if (you == "scissors") {
+            if (opponent == "paper") {
+                yourScore += 1;
+            }
+            else if (opponent == "rock") {
+                opponentScore += 1;
+            }
+        } else if (you == "paper") {
+            if (opponent == "rock") {
+                yourScore += 1;
+            }
+            else if (opponent == "scissors") {
+                opponentScore += 1;
+            }
+        }
+    }
+
+    document.getElementById("your-score").innerText = yourScore;
+    document.getElementById("opponent-score").innerText = opponentScore
 }
