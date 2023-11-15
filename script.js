4aa87bf878a38ff0d5d27f159d33e876f0b2c595
Zaunsrevenge <123345261+Zaunsrevenge@users.noreply.github.com>
2023-11-15 02:34:00 +0000

    added clickable choices and random pc choice

---
+++ script.js
@@ -0,0 +1,25 @@
+let you;
+let yourScore = 0;
+let opponent;
+let opponentScore = 0;
+
+let choices = ["rock", "paper", "scissors"];
+window.onload = function () {
+    for (let i = 0; i < 3; i++) {
+        // <img id="rock" src="rock.png">
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
+        document.getElementById("choices").append(choice);
+    }
+}
+
+function selectChoice() {
+    you = this.id;
+    document.getElementById("your-choice").src = you + ".png";
+
+    //random for opponent       
+    opponent = choices[Math.floor(Math.random() * 3)]; //0- .999999 * 3 = 0-2.99999
+    document.getElementById("opponent-choice").src = opponent + ".png";
+}
